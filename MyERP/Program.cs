1faf947c68d9604a7b6141510103cba8564bda12
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-09-15 14:14:01 +0300

    001-Configuration were added.

--- MyERP/Program.cs
+++ MyERP/Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// add jwt bearer - appsettings
+
+// rate limiter
+
+// add output cache
+
+// appdbcontext - appsettings.json communication will create
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// use authentication
 
 app.UseAuthorization();
 
