2a8a3b3f624fe40d085de456f62973d6e4879e82
Oguzhan Kaya <oguzhankayabusiness@gmail.com>
2024-09-15 15:21:51 +0300

    002-Models were added.

---
+++ MyERP.Core/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyERP.Core.Models
+{
+    public class Customer : BaseEntity
+    {
+        // One customer has many payments and one payment have one customer.
+        // One customer has many sales and one sale have one customer.
+        //One to Many Relationship
+
+        public string Name { get; set; }
+        public ICollection<Payment> Payments { get; set; }
+        public ICollection<Sale> Sales { get; set; }
+    }
+}
